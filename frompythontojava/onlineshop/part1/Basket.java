ba38d5f707e87b8fab83dc50e7753f2a53a44ec7
LMalko86 <codewars@onet.pl>
2017-12-18 14:20:19 +0100

    Iterator pattern

--- frompythontojava/onlineshop/part1/Basket.java
+++ frompythontojava/onlineshop/part1/Basket.java
@@ -1,3 +1,30 @@
+import java.util.ArrayList;
+
 public class Basket{
+
+    private Iterator iterator;
+    private ArrayList<Product> productList;
+
+    private class ProductIterator implements Iterator{
+
+        @Override
+        public boolean hasNext() {
+            
+               if(index < names.length){
+                  return true;
+               }
+               return false;
+            }
+        
+        @Override
+        public Object next() {
+            
+               if(this.hasNext()){
+                  return names[index++];
+               }
+               return null;
+            }	
+
+    }
     
 }
