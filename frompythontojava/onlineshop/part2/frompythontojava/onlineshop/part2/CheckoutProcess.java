f18f2cb4200d04ac90770faef20999539307b46a
LMalko86 <33723325+LMalko86@users.noreply.github.com>
2017-12-21 14:09:43 +0100

    Delete CheckoutProcess.java

--- frompythontojava/onlineshop/part2/frompythontojava/onlineshop/part2/CheckoutProcess.java
+++
@@ -1,9 +0,0 @@
-package frompythontojava.onlineshop.part2;
-
-public class CheckoutProcess extends AbstractProcess{
-
-    protected void action(Orderable item){
-        item.checkout();
-    }
-
-}
